bbba443dc533b295f161e64bb4c9e694e5622ae2
jneilliii <jneilliii+octoprint@gmail.com>
2019-08-23 13:49:34 -0400

    Changed things around to utilize knockout bindings instead of jQuery DOM manipulation/injection.

--- octoprint_webcamSB/__init__.py
+++ octoprint_webcamSB/__init__.py
@@ -4,8 +4,8 @@ from __future__ import absolute_import
 import octoprint.plugin
 
 class WebcamSBPlugin(octoprint.plugin.StartupPlugin,
-               octoprint.plugin.AssetPlugin,
-               octoprint.plugin.TemplatePlugin,
+			   octoprint.plugin.AssetPlugin,
+			   octoprint.plugin.TemplatePlugin,
 			   octoprint.plugin.SettingsPlugin):
 
 	def on_after_startup(self):
@@ -15,25 +15,26 @@ class WebcamSBPlugin(octoprint.plugin.StartupPlugin,
 			orden_sidebar.remove("plugin_webcamSB")
 		nuevo_orden = ["plugin_webcamSB"] + orden_sidebar
 		self._settings.global_set(["appearance","components","order","sidebar"],nuevo_orden)
-	
+
 	def on_plugin_enabled(self):
 		pass
 
-	def get_settings_defaults(self):		
-		return dict(url="",url1="",url2="",url3="",expand=1,defaultCam=1)
+	def get_settings_defaults(self):
+		return dict(url="",url1="",url2="",url3="",expand=1,defaultCam=1,urls=[])
 
 	def get_assets(self):
 		return dict(
 			js=["js/webcamsb.js"],
 			css=["css/webcamsb.css"]
 		)
-		
+
 	def get_template_configs(self):
 		return [
-			dict(type="sidebar", name="Stream", template="webcamsb_sidebar.jinja2", custom_bindings=False),
-			dict(type="settings", name="Sidebar Webcam", template="webcamsb_settings.jinja2", custom_bindings=False)
+			dict(type="sidebar", name="Stream", template="webcamsb_sidebar.jinja2", icon="camera", custom_bindings=True),
+			dict(type="settings", name="Sidebar Webcam", template="webcamsb_settings.jinja2", custom_bindings=True),
+			dict(type="generic", name="Full Screen", template="webcamsb.jinja2", custom_bindings=False)
 		]
-	
+
 	def get_template_vars(self):
 		return dict(webcamStream=self._settings.global_get(["webcam","stream"]))
 
--- octoprint_webcamSB/static/js/webcamsb.js
+++ octoprint_webcamSB/static/js/webcamsb.js
@@ -5,100 +5,49 @@
  * License: AGPLv3
  */
 $(function() {
-    function WebcamSBViewModel(parameters) {
-        var self = this;
-		self.settings = parameters[0];		
-		self.expanded_cam = ko.observable(false); //Change suggested by jneilliii
-		self.onBeforeBinding = function() {
-			$('#webcamsb').html('<img id="sidewebcam" src="" data-bind="click: wcsbPOC">');
-			var u = [self.settings.settings.plugins.webcamSB.url(),self.settings.settings.plugins.webcamSB.url1(),self.settings.settings.plugins.webcamSB.url2(),self.settings.settings.plugins.webcamSB.url3()];
-			var num_cams = 0;
-			var def_cam = self.settings.settings.plugins.webcamSB.defaultCam();
-			if (u[0] == "") u[0] = $('#settings-webcamStreamUrl').val();
-			if (u[1] != "" || u[2] != "" || u[3] != "") { 
-				$('#webcamsb').prepend('<div id="webcsb_botones"></div>');
-			}
-			if (u[0] != "") {
-				$('#webcsb_botones').append('<button class="btn" data-bind="click: wcsbSC1">Cam 1</button>');
-				num_cams++;
-			}
-			if (u[1] != "") {
-				$('#webcsb_botones').append('<button class="btn" data-bind="click: wcsbSC2">Cam 2</button>');
-				num_cams++;
-			}			
-			if (u[2] != "") {
-				$('#webcsb_botones').append('<button class="btn" data-bind="click: wcsbSC3">Cam 3</button>');
-				num_cams++;
-			}
-			if (u[3] != "") {
-				$('#webcsb_botones').append('<button class="btn" data-bind="click: wcsbSC4">Cam 4</button>');
-				num_cams++;
-			}		
-			if (num_cams < 2) $('#webcsb_botones').remove();
-			if (num_cams == 0) {
-				$('#webcamsb').html("<p style='font-size:0.9em;text-align:center'>Stream not set</p>"); return;
-			}
-			var x = 3;
-			while (x >= 0) {
-				if (def_cam == x+1 && u[x] == "") {
-					def_cam--; x--; continue; 
-				} else {
-					self.wcsb_cargaCam(def_cam);
-					break;
-				}
-				x--;
-			}
+	function WebcamSBViewModel(parameters) {
+		var self = this;
+		self.settings = parameters[0];
+		self.expanded_cam = ko.observable(false);
+		self.current_cam_url = ko.observable('');
+		self.current_cam_name = ko.observable('');
+		self.getByName = function(name) {
+			return ko.utils.arrayFirst(self.settings.settings.plugins.webcamSB.urls(), function(item) {
+				return item.name() === name;
+			}) || {name:ko.observable('Default'),url:self.settings.webcam_streamUrl};
 		};
-		self.wcsb_cargaCam = function(cual) {
-			var str = "";
-			if (cual == 1) {
-				str = self.settings.settings.plugins.webcamSB.url();
-				if (str == "") str = $('#settings-webcamStreamUrl').val();
-			}
-			if (cual == 2) str = self.settings.settings.plugins.webcamSB.url1();
-			if (cual == 3) str = self.settings.settings.plugins.webcamSB.url2();
-			if (cual == 4) str = self.settings.settings.plugins.webcamSB.url3();
-			var lacam = $("#sidewebcam"); 
-			str += "?" + new Date().getTime();
-			lacam.attr("src", str);	
-		}		
-		self.wcsbSC1 = function() { self.wcsb_cargaCam(1); }
-		self.wcsbSC2 = function() { self.wcsb_cargaCam(2); }
-		self.wcsbSC3 = function() { self.wcsb_cargaCam(3); }
-		self.wcsbSC4 = function() { self.wcsb_cargaCam(4); }
-		self.wcsbPOC = function() {
-			var posl = 0; var post = 0;
-			var tamw = '100%';
-			var pos = 'relative';
-			var tamh = 'auto';
+
+		self.onAfterBinding = function() {
+			self.wcsbChangeCam();
+		};
+
+		self.wcsbChangeCam = function(data) {
+			var camera = self.getByName(self.settings.settings.plugins.webcamSB.defaultCam());
+			var camera_url = camera.url();
+			camera_url += (camera_url.indexOf('?') < 0) ? '?' : '&';
+			camera_url += new Date().getTime();
+			self.current_cam_url(camera_url);
+			self.current_cam_name(camera.name());
+		}
+
+		self.wcsbPOC = function(data) {
 			if (self.settings.settings.plugins.webcamSB.expand() == 1) {
-				if (self.expanded_cam()) { 
-					tamw = '100%'; tamh = 'auto'; pos = 'relative'; posl= 0; post = 0;
-					$('#wcsb_bk').remove();
-					self.expanded_cam(false);
-				} else { 
-					tamw = 'auto'; tamh = ($(window).height()-20)+'px'; pos = 'fixed'; posl = 0; post = 10;
-					$('#webcamsb').append('<div id="wcsb_bk"></div>');
-					$('#wcsb_bk').css({"display":"block","position":"fixed","background-color":"#000","left":0,"top":0,"width":"100%","height":"100%","z-index":9});
-					$('#wcsb_bk').fadeTo("fast", 0.50);	
-					self.expanded_cam(true);
-					//self.expanded = 1;
-				}
-				$('#webcamsb').css({"position":"relative"});
-				$('#sidewebcam').css({"display":"block","position":pos,"z-index":10,"width":tamw,"height":tamh,"left":posl,"top":post});
-				if (self.expanded_cam()) {
-					posl=(($(window).width()-$('#sidewebcam').width())/2);
-					$('#sidewebcam').css("left",posl);
-				}
+				$('div#webcsb_large').modal("show");
 			}
 		}
-		self.onAfterBinding = function() {
-			$('#sidebar_plugin_webcamSB_wrapper .accordion-heading a').prepend('<i class="fa icon-black fa-camera"/>');		
-        };			
-    };
-    OCTOPRINT_VIEWMODELS.push({
-        construct: WebcamSBViewModel,
-        dependencies: ["settingsViewModel"],
-        elements: ["#sidebar_plugin_webcamSB_wrapper"]
-    });
+
+		self.webcamsb_add_cam = function() {
+			self.settings.settings.plugins.webcamSB.urls.push({name:ko.observable(''),url:ko.observable('')});
+		}
+
+		self.webcamsb_remove_cam = function(data) {
+			self.settings.settings.plugins.webcamSB.urls.remove(data);
+		}
+	};
+
+	OCTOPRINT_VIEWMODELS.push({
+		construct: WebcamSBViewModel,
+		dependencies: ["settingsViewModel"],
+		elements: ["#sidebar_plugin_webcamSB_wrapper","div#webcsb_large","#settings_plugin_webcamSB"]
+	});
 });
